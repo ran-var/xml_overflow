0dd9220954c804bbe767846ba1e48d9e37fd8a30
ran-var <ran.varshaver1@gmail.com>
2025-09-16 15:27:41 +0300

    vulnerable code example

--- parser.c
+++ parser.c
@@ -2,14 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <libxml/parser.h>
-
-typedef struct {
-    char username[32];
-    char password[16];
-    char device_id[64];
-    char wifi_ssid[32];
-    int config_version;
-} config_t;
+#include "parser.h"
 
 int parse_config(const char* filename, config_t* cfg) {
     xmlDocPtr doc = xmlParseFile(filename);
